a793dc1221b50879f9868e238767bb7fb74069b9
sor4chi <sorakawa0218@gmail.com>
2023-11-15 07:25:01 +0900

    chore: init cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin()]
   },
+  output: "server",
+  adapter: cloudflare()
 });
