fb82e4c9f119ca01c7ecc3d118f67311b1a0c203
sor4chi <sorakawa0218@gmail.com>
2023-11-17 11:32:22 +0900

    feat: create some user-related tables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+type ENV = {
+  DB: D1Database;
+  GITHUB_CLIENT_ID: string;
+  GITHUB_CLIENT_SECRET: string;
+};
+
+type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+  }
+}
