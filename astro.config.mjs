93e8a7cb445d13a13c4611da2a4ccb9900099f8e
sor4chi <sorakawa0218@gmail.com>
2023-11-18 03:12:24 +0900

    feat: add react and radix-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin()]
   },
   output: "server",
   adapter: cloudflare({
-    runtime: { mode: "local" },
+    runtime: {
+      mode: "local"
+    }
   }),
+  integrations: [react()]
 });
