403dd4c77ee665270cde41fc3d05e6647f96884f
sor4chi <sorakawa0218@gmail.com>
2023-11-19 02:59:45 +0900

    feat: exclude optionを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,26 @@ import rehypeCodeBlockCopy from "./src/libs/rehype-code-block-copy";
 import rehypeHeadLinker from "./src/libs/rehype-head-linker";
 import rehypeAnnotationBlock from "./src/libs/rehype-annotation-block";
 
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [vanillaExtractPlugin()]
+    plugins: [vanillaExtractPlugin()],
   },
   output: "server",
   markdown: {
     syntaxHighlight: "prism",
-    rehypePlugins: [rehypeCodeBlockCopy, rehypeHeadLinker, rehypeAnnotationBlock]
+    rehypePlugins: [
+      rehypeCodeBlockCopy,
+      rehypeHeadLinker,
+      rehypeAnnotationBlock,
+    ],
   },
   adapter: cloudflare({
     runtime: {
-      mode: "local"
-    }
+      mode: "local",
+    },
   }),
-  integrations: [react()]
+  integrations: [react()],
 });
