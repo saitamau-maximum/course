86ad6309151ef42f5a57d87b45081a392dea8dc6
sor4chi <sorakawa0218@gmail.com>
2023-11-19 02:32:00 +0900

    feat: meta 設定

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,13 @@
 export const CONTENT_INDEX_MATCHER = /(.*\/)?(.*)\/index\.mdx?$/;
 export const CONTENT_SECTION_MATCHER = /(.*\/)?(.*)\/(.*)\.mdx?$/;
+
+export const SITE_TITLE = "Maximum Course";
+
+export const CANONICALS = {
+  HOME: "/",
+  COURSE: "/course",
+  COURSE_DETAIL_TOP: (courseSlug: string) => `/course/${courseSlug}`,
+  COURSE_DETAIL: (courseSlug: string, sectionSlug: string) =>
+    `/course/${courseSlug}/${sectionSlug}`,
+  PROFILE: "/profile",
+};
